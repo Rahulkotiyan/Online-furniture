4e8e3c8826e5c0ef800fe6d224a776c9deef88d3
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-06-08 12:34:38 +0530

    furniture

---
+++ FURBITURE/php/connect.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+    $connection = mysqli_connect("localhost", "root", "");
+    if (!$connection) die("<ERROR: Cannot connect to database>");
+    $database = mysqli_select_db($connection, "furniture_db");
+    if (!$database) die("<ERROR:Cannot select database>");
