d1a695b740b6855bfb0f7c301a29ef8ef82a2f3f
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-06-08 12:57:08 +0530

    Update connect.php

--- FURBITURE/php/connect.php
+++ FURBITURE/php/connect.php
@@ -3,5 +3,5 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
     $connection = mysqli_connect("localhost", "root", "");
     if (!$connection) die("<ERROR: Cannot connect to database>");
-    $database = mysqli_select_db($connection, "furniture");
+    $database = mysqli_select_db($connection, "furbiture");
     if (!$database) die("<ERROR:Cannot select database>");
